5348c5614dd020f000a5ff7d5b6866e8db64a050
JimButseraen <jim.butseraen@gmail.com>
2015-04-21 17:00:30 +0200

    Models in apart project

--- Webshop/Week 4,5,6/WebshopEF/WebshopEF/Models/IdentityModels.cs
+++ Webshop/Week 4,5,6/WebshopEF/WebshopEF/Models/IdentityModels.cs
@@ -7,24 +7,6 @@ using Microsoft.AspNet.Identity.EntityFramework;
 namespace WebshopEF.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
-        public string Name { get; set; }
-        public string FirstName { get; set; }
-        public string Address { get; set; }
-        public string ZipCode { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
-
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
-    }
-
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Device> Device { get; set; }
